04c3517d8aea32b3f74ef3ce9fb3209cca34da2d
lzyprime <2383518170@qq.com>
2021-07-05 10:40:40 +0800

    文章处理脚本

---
+++ source/shell_scripts/post.sh
@@ -0,0 +1,16 @@
+function dfs() {
+	for i in `ls`; do
+		if [ -d $i ]; then
+			cd $i
+			dfs
+			cd ../
+		elif [[ $i =~ .*.md$ ]]; then 
+			filename=$(basename $i .md)
+			sed -i "s#($filename/#(../$filename/#" $i
+			echo $filename finish
+		fi
+	done
+}
+
+cd ../../source
+dfs
